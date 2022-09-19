624502939545d833d408841c98a6c2f6776aa5f1
Tlafay1 <tlclash12@gmail.com>
2022-09-19 17:52:12 -0400

    Still struggling on autocompletion

--- prompt.py
+++ prompt.py
@@ -1,9 +1,10 @@
 import readline
+import os
 
 class InteractivePrompt:
 
 	def __init__(self):
-		self.options = sorted(['shell', 'webcam', 'download', 'upload', 'exit'])
+		self.commands = sorted(['shell', 'webcam', 'download', 'upload', 'exit'])
 		return
 
 	def input(self, msg):
@@ -18,17 +19,34 @@ class InteractivePrompt:
 
 		return cmd.strip()
 
+	def get_cur_before(self):
+		idx = readline.get_begidx()
+		full = readline.get_line_buffer()
+		return full[:idx]
+
+	# def get_current(self):
+	# 	return readline.get_line_buffer()[readline.get_begidx():readline.get_endidx()]
+
 	def complete(self, text, state):
-		response = None
-		if state == 0:
-			if text:
-				self.matches = [s 
-				for s in self.options
-				if s and s.startswith(text)]
-			else:
-				self.matches = self.options[:]
-		try:
-			response = self.matches[state]
-		except IndexError:
-			response = None
-		return response
+		# last_slash = text.rfind('/')
+		# if last_slash == -1:
+		# 	path = text
+		# else:
+		# 	path = text[:last_slash]
+		# 	text = text[last_slash + 1:]
+		# if text:
+		# 	server_files = os.listdir(path)
+		# else:
+		server_files = os.listdir('.')
+		if len(self.get_cur_before().split(" ")) <= 1:
+			results = [x + " " for x in self.commands if x.startswith(text)] + [None]
+		else:
+			results = [x + " " for x in server_files if x.startswith(text)] + [None]
+		return results[state]
+
+
+# if text:
+# 	text = text.substr(0, text.rfind('/'))
+# 	server_files = os.listdir(text)
+# else:
+# 	server_files = os.listdir('.')
